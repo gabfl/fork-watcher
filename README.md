a5c00f0f958a7c792bd33e71b2d876e0ff45c06d
gab <gabrielbordeaux@gmail.com>
2018-12-13 19:01:37 -0500

    Form watcher v1.0

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # fork-watcher
+
+This simple tool loops thru your repositories and watches all forks.
+
+## Usage
+
+Generate a Github token at https://github.com/settings/tokens and select the following scopes:
+   * `repo -> public_repo`
+   * `notifications`
+ 
+ Export the token and run the program:
+
+```bash
+ export GH_TOKEN=some_token
+ python3 fork-watcher.py
+ ```
+
+## Example:
+
+```bash
+$ export GH_TOKEN=some_token
+
+$ python3 fork-watcher.py 
+You are currently watching 27 repos
+ * repo: dbschema
+  ...watching TheLady/dbschema -> success
+ * repo: mysql_generate_series
+  ...watching Youlina3/mysql_generate_series -> success
+  ...watching blackberry-bb/mysql_generate_series -> success
---
+++ fork-watcher.py
@@ -0,0 +1,111 @@
+import requests
+import json
+import os
+
+
+def set_token():
+    """ Read token from env variable end set it """
+
+    global token
+
+    try:
+        token = os.environ['GH_TOKEN']
+    except KeyError:
+        raise RuntimeError(
+            'Missing Github token. Use `export GH_TOKEN=my_token`')
+
+
+def get_subscriptions():
+    """ Get my current subscriptions"""
+
+    subscriptions = requests.get('https://api.github.com/user/subscriptions',
+                                 auth=('token', token))
+    subscriptions = subscriptions.json()
+
+    if subscriptions:
+        subscriptions = [subscription['full_name']
+                         for subscription in subscriptions]
+
+        print('You are currently watching %d repos' % len(subscriptions))
+
+        return subscriptions
+
+    return []
+
+
+def get_forks(forks_url):
+    """ Fetch and return forks """
+
+    # Retrieve the list of forks
+    forks = requests.get(forks_url,
+                         auth=('token', token))
+
+    return forks.json()
+
+
+def subscribe(subscription_url):
+    """ Subscribe to a repository """
+
+    # Prepare subscription payload
+    payload = {
+        'subscribed': True,
+        'ignored': False
+    }
+
+    # Subscribe
+    subscription = requests.put(
+        subscription_url,
+        auth=('token', token),
+        data=json.dumps(payload)
+    )
+
+    return True if subscription.status_code >= 200 and subscription.status_code < 300 else False
+
+
+def get_repos():
+    """ Get the list of repositories excluding forks """
+
+    repos = requests.get('https://api.github.com/user/repos',
+                         auth=('token', token))
+    repos = repos.json()
+
+    if repos:
+        return [repo for repo in repos if repo['fork'] is False]
+
+    return []
+
+
+def search_and_subscribe():
+    """ Loop thru repositories, fetch forks and subscribe """
+
+    repos = requests.get('https://api.github.com/user/repos',
+                         auth=('token', token))
+
+    for repo in get_repos():
+        print(' * repo: %s' % (repo['name']))
+
+        # Loop thru forks
+        for fork in get_forks(repo['forks_url']):
+            if fork['full_name'] not in current_subscriptions:
+                # Attempt to subscribe
+                res = subscribe(fork['subscription_url'])
+
+                print('  ...watching %s -> %s' %
+                      (fork['full_name'], 'success' if res is True else 'failed'))
+
+
+def main():
+    global current_subscriptions, token
+
+    # Read token from env
+    set_token()
+
+    # Get my current subscriptions
+    current_subscriptions = get_subscriptions()
+
+    # Loop thru forks and subscribe
+    search_and_subscribe()
+
+
+if __name__ == '__main__':
+    main()
